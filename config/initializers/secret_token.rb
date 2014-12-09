2dddf9dd48cf881cf3c8088feed510d241699115
Koutarou Fujii <fujii@aica.ac.jp>
2014-12-10 08:58:21 +0900

    空のプロジェクト

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hondana::Application.config.secret_key_base = 'a5174bc4c65d279655619693c8709a30f033d3c6029f0c0b8429ff9b27f5eeff947c31c2e03cc37c494e38b6c37618ec54d968935dda92d6c576069c82e74f44'
