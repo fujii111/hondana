0ffc69144f8cadb6528ee028d0a7e3147b6e601c
Koutarou Fujii <fujii@aica.ac.jp>
2014-12-10 09:58:19 +0900

    コメントテスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # kensuke_test
-  # fujii_test
+  # fujii111_test
   protect_from_forgery with: :exception
 end
