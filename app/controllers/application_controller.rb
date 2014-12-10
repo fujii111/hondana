57b352fa05e312af9b3a0987bb7d1db2ad14446b
naoya suzuki <shou_hei_hei2222@inter7.jp>
2014-12-10 10:17:35 +0900

    名前追加しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # ken_test
-  # fujii111_test
+  # fujii111_test#
+  # aica_suzuki
   protect_from_forgery with: :exception
 end
