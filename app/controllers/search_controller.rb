a0d15c1e609d46e44bf66549feac93380cb9ebc2
hskrk <236236plusb@gmail.com>
2014-12-26 14:09:33 +0900

    2014年最後のコミット_保坂

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,21 +4,24 @@ class SearchController < ApplicationController
   end
 
   def index
+     
     @keyword = params['keyword']
     httpClient = HTTPClient.new
 
     @jsonData = nil
     @errorMeg = nil
-
+    
+    @bookinfo = Bookinfo.where(name: '書籍').select(:name)
+    
+    
     begin
       data = httpClient.get_content('https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522', {
           'applicationId' => '1029724767561681573',
           'affiliateId'   => '12169043.4164998a.12169044.3519539e',
           'format'        => 'json',
-          'elements'      => 'count,page,title,author,publisherName,isbn,itemCaption,salesDate,itemUrl,mediumImageUrl,booksGenreName',
+          'elements'      => 'count,page,first,last,pageCount,title,author,publisherName,isbn,itemCaption,salesDate,itemUrl,mediumImageUrl,booksGenreName',
           'title'         => @keyword,
           'hits'          => '10',
-          'page'          => '10',
       })
       @jsonData = JSON.parse data
       p @jsonData
