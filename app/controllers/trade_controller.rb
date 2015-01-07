13e69d7fe238c9031550909ea8c8a9b817c1592a
palwo <nyanparu@gmail.com>
2015-01-07 11:47:12 +0900

    trade進みました

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -4,16 +4,17 @@ class TradeController < ApplicationController
   def index
   end
   def select
-    # @member = Member.find(params[:id])
     @books = Book.all
+    #booksのmembers_idからmemberのlogin_idを取得
+    @owner = Member.find_by_sql("SELECT login_id FROM members ,books WHERE members.id = books.members_id")
   end
   def confirm
-    @books = Book.all
+
   end
   def comp
-    @books = Book.all
+
   end
   def details
-    @books = Book.all
+
   end
 end
