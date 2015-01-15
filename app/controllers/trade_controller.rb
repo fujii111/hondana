61bb07338782401b6d71aa56a21641d22347318a
palwo <nyanparu@gmail.com>
2015-01-15 09:52:41 +0900

    昨日の修正分

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -4,9 +4,7 @@ class TradeController < ApplicationController
   def index
   end
   def select
-    @books = Book.all
-    #booksのmembers_idからmemberのlogin_idを取得
-    @owner = Member.find_by_sql("SELECT login_id FROM members ,books WHERE members.id = books.members_id")
+    @books = Book.find_by_sql("SELECT * FROM members ,books WHERE books_flag = 0 AND members.id = books.members_id")
   end
   def confirm
     @books = Book.all
