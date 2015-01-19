9781fbc347c366749b0e62904692cb079626d42b
aica1210tog <hogehoge0530@gmai.com>
2015-01-19 10:02:24 +0900

    profile微修正

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,16 +1,14 @@
 class ProfileController < ApplicationController
-  #require 'digest/sha1'
-  #require 'bcrypt'
 
-  $id = nil
-  $member = nil
+  @id = nil
+  @member = nil
   def index
     if session[:id].nil? then
       redirect_to "/signin/"
     else
-      $id = session[:id]
-      $member = Member.includes(:Favorite_authors).where(:members_id => $id).references(:Favorite_authors)
-      get_member($id)
+      @id = session[:id]
+      @member = Member.includes(:Favorite_authors).where(:members_id => @id).references(:Favorite_authors)
+      get_member(@id)
     end
 
   end
@@ -19,12 +17,12 @@ class ProfileController < ApplicationController
     if session[:id].nil? then
       redirect_to "/signin/"
     else
-      $id = session[:id]
-      get_member($id)
+      @id = session[:id]
+      get_member(@id)
       get_ref
       if @refs == "profile" then#profile/indexから来てる時だけ処理
         session[:prof] = params[:prof]
-        session[:prof][:pass] = $member.password_digest
+        session[:prof][:pass] = @member.password_digest
         genre_find
       elsif @refs == "confirm" then
         #確認画面で戻るボタンを押した場合
@@ -43,7 +41,7 @@ class ProfileController < ApplicationController
     get_ref
     if @refs == "edit" then
       #editから来てる時だけ処理
-      $id = session[:id]
+      @id = session[:id]
       if params[:prof][:name] == "" || params[:prof][:name_kana] == "" || params[:prof][:nickname] == "" || params[:prof][:mail] == "" || params[:prof][:pass] == "" || params[:prof][:birth_year] == "" || params[:prof][:birth_month] == "" || params[:prof][:birth_day] == "" || params[:prof][:address] == "" then
         #入力内容に空欄がある
         redirect_to({action: :edit} , notice: 'empty')
@@ -60,26 +58,26 @@ class ProfileController < ApplicationController
     get_ref
     if @refs == "confirm" then#confirmから来てる時だけ更新処理
 
-      $id = session[:id]
-      get_member($id)
+      @id = session[:id]
+      get_member(@id)
       @birth = session[:prof][:birth_year] + session[:prof][:birth_month] + session[:prof][:birth_day]
 
       #profileの更新(パスワードは未着手)
-      $member.update(name: session[:prof][:name],kana: session[:prof][:name_kana],nickname: session[:prof][:nickname],mail_address: session[:prof][:mail], address: session[:prof][:address], birthday:@birth)
+      @member.update(name: session[:prof][:name],kana: session[:prof][:name_kana],nickname: session[:prof][:nickname],mail_address: session[:prof][:mail], address: session[:prof][:address], birthday: @birth)
 
       #favorite_authorの更新作業
       @arr = [session[:prof][:fav_author_0], session[:prof][:fav_author_1], session[:prof][:fav_author_2]]
 
       for num in 0..2 do
-        if !!$member.favorite_authors.find_by(members_id:$id, sort:num + 1) then#favorite_authorがnilじゃないことを確認
+        if !!@member.favorite_authors.find_by(members_id:@id, sort:num + 1) then#favorite_authorがnilじゃないことを確認
           if @arr[num] != '' then#sessionの:fav_authorが空欄じゃないことを確認
-            $member.favorite_authors[num].update(author:@arr[num])#空じゃなければ挿入
+            @member.favorite_authors[num].update(author:@arr[num])#空じゃなければ挿入
           else#空だった場合...存在したカラムを空欄にした
-            $member.favorite_authors[num].delete()
+            @member.favorite_authors[num].delete()
           end
         else#favorite_authorがnil
           if @arr[num] != '' then#:fav_authorが空欄じゃないことを確認
-            $member.favorite_authors.create(members_id:$id, author:@arr[num], sort:num + 1)#空じゃなければ作成
+            @member.favorite_authors.create(members_id:@id, author:@arr[num], sort:num + 1)#空じゃなければ作成
           end
         end
       end
@@ -88,7 +86,7 @@ class ProfileController < ApplicationController
       @arr = [session[:prof][:fav_genre_0], session[:prof][:fav_genre_1], session[:prof][:fav_genre_2]]
       @mem =[3]
       for num in 0..2 do
-        @mem[num] = MembersGenre.find_by(members_id:$id, sort:num + 1)
+        @mem[num] = MembersGenre.find_by(members_id:@id, sort:num + 1)
 
         if !!@mem[num] then#favorite_authorがnilじゃないことを確認
           if @arr[num] != '0' then#sessionの:fav_authorが「なし」じゃないことを確認
@@ -98,7 +96,7 @@ class ProfileController < ApplicationController
           end
         else#members_genresがnilの場合
           if @arr[num] != '0' then#:fav_genresが「なし」じゃないことを確認
-            MembersGenre.create(members_id:$id, bookgenres_id:@arr[num], sort:num + 1)#空じゃなければ作成
+            MembersGenre.create(members_id:@id, bookgenres_id:@arr[num], sort:num + 1)#空じゃなければ作成
           end
         end
       end
@@ -137,7 +135,7 @@ class ProfileController < ApplicationController
   end
 
   def get_member(numid)#変数の初期化
-    $member = Member.find(numid)
+    @member = Member.find(numid)
     @genres = Bookgenre.all
     @genre_name = Bookgenre.find_by_sql(['select members_genres.members_id,members_genres.bookgenres_id,bookgenres.name from bookgenres join members_genres on members_genres.bookgenres_id=bookgenres.id where members_genres.members_id=:numid', {:numid=> numid}])
   end
