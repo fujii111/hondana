25cf570cb13c6303c8f017fa7ce18b2cf38e380d
aica1210tog <hogehoge0530@gmai.com>
2015-01-20 12:02:32 +0900

    表示のための下準備

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -1,5 +1,15 @@
 class CollectionController < ApplicationController
+
+  @id = nil
+  @books = nil
+
   def index
+    if session[:id].nil? then
+      redirect_to "/signin/"
+    else
+      @id = session[:id]
+      @books = Books.find_by(members_id:@id)
+    end
   end
   def confirm
   end
