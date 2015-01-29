7920889afaaf70d9dfe5ece5173b98c86df9ae9e
hskrk <236236plusb@gmail.com>
2015-01-29 09:22:48 +0900

    こみっとお

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -28,8 +28,8 @@ class SearchController < ApplicationController
     #session[:keyword] = @keyword
 
     @keyword = params['keyword']
-
-
+    
+    
     if @keyword == "" or @keyword =~ /^[\s　]+$/ then
       @nilKeyword = 0
       return
@@ -38,28 +38,32 @@ class SearchController < ApplicationController
       return
     else
 
-    httpClient = HTTPClient.new
+      httpClient = HTTPClient.new
+  
+      @jsonData = nil
+      @errorMeg = nil
+      
+      @bookinfo = Bookinfo.where("name like '%" + @keyword + "%' or author like '%" + @keyword + "%'")
+  
+      @nilKeyword = 2
+      
 
-    @jsonData = nil
-    @errorMeg = nil
+        begin
+          data = httpClient.get_content('https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522', {
+              'applicationId' => '1029724767561681573',
+              'affiliateId'   => '12169043.4164998a.12169044.3519539e',
+              'format'        => 'json',
+              'elements'      => 'count,page,first,last,pageCount,title,author,publisherName,size,isbn,itemCaption,salesDate,itemUrl,mediumImageUrl,booksGenreName',
+              'title'         => @keyword,
+              'hits'          => '10',
+          })
 
-    @bookinfo = Bookinfo.where("name like '%" + @keyword + "%' or author like '%" + @keyword + "%'")
-
-    @nilKeyword = 2
-
-    begin
-      data = httpClient.get_content('https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522', {
-          'applicationId' => '1029724767561681573',
-          'affiliateId'   => '12169043.4164998a.12169044.3519539e',
-          'format'        => 'json',
-          'elements'      => 'count,page,first,last,pageCount,title,author,publisherName,size,isbn,itemCaption,salesDate,itemUrl,mediumImageUrl,booksGenreName',
-          'title'         => @keyword,
-          'hits'          => '10',
-      })
-      @jsonData = JSON.parse data
-      p @jsonData
-    rescue HTTPClient::BadResponseError => e
-    rescue HTTPClient::TimeoutError => e
+        
+        @jsonData = JSON.parse data
+        p @jsonData
+        
+     rescue HTTPClient::BadResponseError => e
+     rescue HTTPClient::TimeoutError => e
     end
     render 'search/index'
     end
