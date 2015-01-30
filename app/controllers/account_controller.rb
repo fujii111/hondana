4502a7f5f2ab97e2c235a4b380487ed7d16779de
aica1210tog <hogehoge0530@gmai.com>
2015-01-30 12:33:09 +0900

    お気に入りとかログインチェックのif文とか

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -6,38 +6,47 @@ class AccountController < ApplicationController
   @member = nil
   @fav_book = nil
   def index
-    @mid = cookies[:id].to_i
-    @member = Member.find(@mid)
+    if cookies[:id].nil? then
+      redirect_to "/signin/"
+    else
+      @mid = cookies[:id].to_i
+      @member = Member.find(@mid)
 
-    #BooksとBookinfosを結合
-    @push_book = Book.find_by_sql(["select * from books join bookinfos on books.bookinfos_id = bookinfos.id where books.books_flag = '0' and books.members_id =:id",{:id => @mid}])
-    #件数表示のためのカウント
-    @count = @push_book.length
-    #MembersBooksとBookinfosを結合
-    @fav_book =MembersBooks.find_by_sql(["select members_books.id, members_books.books_id, bookinfos.name from members_books join bookinfos on members_books.books_id = bookinfos.id where members_books.members_id =:id order by sort",{:id => @mid}])
+      #BooksとBookinfosを結合
+      @push_book = Book.find_by_sql(["select * from books join bookinfos on books.bookinfos_id = bookinfos.id where books.books_flag = '0' and books.members_id =:id",{:id => @mid}])
+      #件数表示のためのカウント
+      @count = @push_book.length
+      #MembersBooksとBookinfosを結合
+      @fav_book =MembersBooks.find_by_sql(["select members_books.id, members_books.books_id, bookinfos.name from members_books join bookinfos on members_books.books_id = bookinfos.id where members_books.members_id =:id order by sort",{:id => @mid}])
 
-    #在庫表示のフラグ格納用配列
-    @arr = Array.new
-    #お気に入りの書籍をeachで1つずつ抽出
-    @fav_book.each do |fav|
-      #count_flagをお気に入りの書籍ごとにリセット
-      @count_flag = 0
-      #書籍IDが一致する蔵書を全抽出
-      @book = Book.where(bookinfos_id: fav.books_id)
-      #蔵書の全抽出
-      @book.each do |flag|
-        #books_flagが0(交換手続きに入っていない)なおかつmembers_idが自分じゃない
-        if flag.books_flag == 0 && flag.members_id != @mid then
-          @count_flag += 1
+      #在庫表示のフラグ格納用配列
+      @arr = Array.new
+      #お気に入りの書籍をeachで1つずつ抽出
+      @fav_book.each do |fav|
+        #count_flagをお気に入りの書籍ごとにリセット
+        @count_flag = 0
+        #書籍IDが一致する蔵書を全抽出
+        @book = Book.where(bookinfos_id: fav.books_id)
+        #蔵書の全抽出
+        @book.each do |flag|
+          #books_flagが0(交換手続きに入っていない)なおかつmembers_idが自分じゃない
+          if flag.books_flag == 0 && flag.members_id != @mid then
+            @count_flag += 1
+          end
+        end
+        #蔵書が存在する、かつ自分以外の交換可能な蔵書がある
+        if Book.exists?(bookinfos_id: fav.books_id) == 1 && @count_flag >= 1 then
+          @arr.push(1)#フラグを1に
+        else
+          @arr.push(0)#フラグを0に
         end
       end
-      #蔵書が存在する、かつ自分以外の交換可能な蔵書がある
-      if Book.exists?(bookinfos_id: fav.books_id) == 1 && @count_flag >= 1 then
-        @arr.push(1)#フラグを1に
-      else
-        @arr.push(0)#フラグを0に
-      end
     end
+  end
+
+  def create
+    @get = params[:books_id]
+    #redirect_to({action: :index})
   end
 
   def delete
